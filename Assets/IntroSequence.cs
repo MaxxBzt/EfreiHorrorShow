de11cf7eb35e78fb7486f8d42331a4aed2fff17b
Rolwen <n.rnolwen@gmail.com>
2025-05-16 17:06:04 +0200

    truc

---
+++ Assets/IntroSequence.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using UnityEngine;
+
+public class IntroSequenceVR : MonoBehaviour
+{
+    public GameObject blinkEffect;  
+    public GameObject introText; 
+
+    void Start()
+    {
+        blinkEffect.SetActive(false);
+        introText.SetActive(false);
+
+        StartCoroutine(PlayIntro());
+    }
+
+    IEnumerator PlayIntro()
+    {
+        blinkEffect.SetActive(true);
+        yield return new WaitForSeconds(4f);
+        blinkEffect.SetActive(false);
+
+        introText.SetActive(true);
+        yield return new WaitForSeconds(20f); 
+    }
+}
